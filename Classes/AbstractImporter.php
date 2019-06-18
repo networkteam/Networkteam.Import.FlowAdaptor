9c075d405e74b7febb783938f5fb9050288709d0
Kai Möller <kai.moeller@networkteam.com>
2019-06-18 22:51:01 +0200

    Adjust logger configuration

--- Classes/AbstractImporter.php
+++ Classes/AbstractImporter.php
@@ -14,24 +14,16 @@ abstract class AbstractImporter extends \Networkteam\Import\AbstractImporter {
      * @var ImportLoggerInterface
      * @Flow\Inject
      */
-    protected $importLogger;
+    protected $logger;
 
     protected function handleException(\Exception $exception)
     {
         $this->importResult->addError(sprintf('%s (Code: %s)', $exception->getMessage(), $exception->getCode()));
 
-        if ($this->importLogger !== NULL) {
-            $this->importLogger->logException($exception);
+        if ($this->logger !== NULL) {
+            $this->logger->critical($exception->getMessage(), ['exception' => $exception]);
         } else {
             throw $exception;
         }
     }
-
-    protected function log($message, $level = LOG_INFO)
-    {
-        if ($this->importLogger !== NULL) {
-            $this->importLogger->log($message, $level);
-        }
-    }
-
 }
