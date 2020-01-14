3221f1a03f7b5f260bb18c0cd236dd07a046ab8c
Kai Möller <kai.moeller@networkteam.com>
2020-01-14 15:41:50 +0100

    refactoring for networkteam/import 2.0

--- Classes/Log/ImportLoggerInterface.php
+++ Classes/Log/ImportLoggerInterface.php
@@ -1,10 +1,6 @@
 <?php
 namespace Networkteam\Import\FlowAdaptor\Log;
 
-/***************************************************************
- *  (c) 2017 networkteam GmbH - all rights reserved
- ***************************************************************/
-
 use Psr\Log\LoggerInterface;
 
 interface ImportLoggerInterface extends LoggerInterface
